c5d40aa5f2c81ea7bbb5281fc02e792a2b40a8a8
pengxingsong <pxswork@163.com>
2021-06-20 20:01:25 +0800

    新增ibatis项目

---
+++ tibatis/src/test/java/com/pxs/jcode/tibatis/TibatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxs.jcode.tibatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
