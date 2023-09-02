56809a2b07f283cbbc47d90d3eacf97279f2b5f4
pengxingsong <ankerp@outlook.com>
2023-09-02 08:51:40 +0800

    变更

---
+++ demo/src/main/java/com/pxs/demo/tt.java
@@ -0,0 +1,15 @@
+package com.pxs.demo;
+
+
+public class tt {
+
+    private  class  tt2{
+        //可以修饰内部类
+    }
+
+    public  void aa(){
+
+    }
+}
+
+
---
+++ demo/src/test/java/com/pxs/demo/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pxs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("运行成功");
+	}
+
+}
